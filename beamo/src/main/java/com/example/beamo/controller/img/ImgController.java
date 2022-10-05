0c639ea9a3d51f5d82f0b7ef87268159ecac9e89
youngkyunIm <yee0230@naver.com>
2022-10-05 15:06:28 +0900

    image 출력

---
+++ beamo/src/main/java/com/example/beamo/controller/img/ImgController.java
@@ -0,0 +1,40 @@
+package com.example.beamo.controller.img;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+@Controller
+public class ImgController {
+
+    @Value("${pwd.image}")
+    private String pwd_img;
+
+    @GetMapping("/img")
+    public ResponseEntity<byte[]> getImage(String fileName){
+        File file = new File(pwd_img + fileName);
+        ResponseEntity<byte[]> result = null;
+
+        try {
+
+            HttpHeaders header = new HttpHeaders();
+
+            header.add("Content-type", Files.probeContentType(file.toPath()));
+
+            result = new ResponseEntity<>(FileCopyUtils.copyToByteArray(file), header, HttpStatus.OK);
+
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return result;
+    }
+}
