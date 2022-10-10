096f1b33621fe996788a123211680aea221041df
youngkyunIm <yee0230@naver.com>
2022-10-10 21:25:44 +0900

    fix : JWT filter

--- beamo/src/main/java/com/example/beamo/config/SecurityConfig.java
+++ beamo/src/main/java/com/example/beamo/config/SecurityConfig.java
@@ -4,17 +4,12 @@ package com.example.beamo.config;
 
 import com.example.beamo.jwt.JwtRequestFilter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
 
--- beamo/src/main/java/com/example/beamo/jwt/JwtRequestFilter.java
+++ beamo/src/main/java/com/example/beamo/jwt/JwtRequestFilter.java
@@ -20,26 +20,21 @@ import java.io.IOException;
 @Component
 public class JwtRequestFilter extends OncePerRequestFilter {
 
-    @Autowired //(2)
+    @Autowired
     UsersRepository userRepository;
-    @Override //(3)
+    @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-
-        //(4)
         String jwtHeader = ((HttpServletRequest)request).getHeader(JwtProperties.HEADER_STRING);
 
-        //(5)
         if(jwtHeader == null || !jwtHeader.startsWith(JwtProperties.TOKEN_PREFIX)) {
             filterChain.doFilter(request, response);
             return;
         }
 
-        //(6)
         String token = jwtHeader.replace(JwtProperties.TOKEN_PREFIX, "");
 
         Long userCode = null;
 
-        //(7)
         try {
             userCode = JWT.require(Algorithm.HMAC512(JwtProperties.SECRET)).build().verify(token)
                     .getClaim("id").asLong();
@@ -52,10 +47,8 @@ public class JwtRequestFilter extends OncePerRequestFilter {
             request.setAttribute(JwtProperties.HEADER_STRING, "유효하지 않은 토큰입니다.");
         }
 
-        //(8)
         request.setAttribute("userCode", userCode);
 
-        //(9)
         filterChain.doFilter(request, response);
     }
 }
